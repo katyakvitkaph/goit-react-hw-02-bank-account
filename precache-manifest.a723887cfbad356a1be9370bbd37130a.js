e2fa6b852b4e4b4e9685512dcf56c7b281374aba
katyakvitkaph <kvitka.katja@gmail.com>
2019-11-25 17:56:45 +0200

    Updates

---
+++ precache-manifest.a723887cfbad356a1be9370bbd37130a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e22cc83aa94de52d8ecb79a6d2efdb",
+    "url": "/goit-react-hw-02-bank-account/./index.html"
+  },
+  {
+    "revision": "9ee2e83ad2ff642bdb34",
+    "url": "/goit-react-hw-02-bank-account/./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "93203d6ee6c928f63795",
+    "url": "/goit-react-hw-02-bank-account/./static/css/main.8b145aa7.chunk.css"
+  },
+  {
+    "revision": "9ee2e83ad2ff642bdb34",
+    "url": "/goit-react-hw-02-bank-account/./static/js/2.6045be36.chunk.js"
+  },
+  {
+    "revision": "93203d6ee6c928f63795",
+    "url": "/goit-react-hw-02-bank-account/./static/js/main.a6ec30c9.chunk.js"
+  },
+  {
+    "revision": "b1e7ebfd5f303853d7e3",
+    "url": "/goit-react-hw-02-bank-account/./static/js/runtime-main.51f3ffa5.js"
+  }
+]);
